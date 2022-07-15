08f7ad45c1a1f35aa3c6c7bfba2b83588c75e8a4
Artur Khamatnurov <arturkh.116@gmail.com>
2022-07-16 01:26:19 +0300

    Fix: postgres conf, bugs
    Try to do first get

--- server/index.js
+++ server/index.js
@@ -1,21 +1,26 @@
-require('dotenv').config();
-const express = require('express')
-const sequelize = require('./db')
-const models = require('./models/models')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const sequelize = require("./db");
+const models = require("./models/models");
 
-const PORT = process.env.PORT || 5000
-const app = express()
-app.use
+const PORT = process.env.PORT;
+const app = express();
+app.use(cors());
+app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "it works" });
+});
+
+app.get;
 const start = async () => {
-    try{
-        await sequelize.authenticate()
-        await sequelize.sync()
-        app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`))
-    }
-    catch(e){
-        console.log(e)
-    }
-}
-
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
 start();
